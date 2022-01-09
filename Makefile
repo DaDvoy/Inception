b2729bcf14f35a9481cf93cb9cc04ba6802d1116
DaDvoy <lmushroo@student.21-school.ru>
2022-01-09 04:25:09 +0300

    all works, but need to fix wp and mariadb

--- Makefile
+++ Makefile
@@ -8,8 +8,16 @@ build: create_volume
 		docker-compose -f $(DOC_FILES) build
 
 up:
-		docker-compose -f $(DOC_FILES) up -d
-con_clear:
-		docker stop $(docker ps -a -q)
-im_clear:
-		docker rmi $(docker images -a) -f
+	docker-compose -f $(DOC_FILES) up -d
+# con_clear:
+# 	docker stop $$(docker ps -a -q)
+# im_clear:
+# 	docker rmi $$(docker images -a) -f
+
+clean:
+	docker stop $$(docker ps -qa); \
+	docker rm $$(docker ps -qa); \
+	docker rmi -f $$(docker images -qa); \
+	docker volume rm $$(docker volume ls -q); \
+#	docker network rm $$(docker network ls -q) \
+	2> /dev/null	
---
+++ srcs/requirements/wordpress/wp.sh
@@ -0,0 +1,23 @@
+    sleep 10
+  mkdir -p /run/php/
+  touch /run/php/php7.3-fpm.pid
+  chown -R www-data:www-data /var/www/*
+  chmod -R 755 /var/www/*
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+  wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  chmod +x wp-cli.phar
+  mv wp-cli.phar /usr/local/bin/wp
+  cd /var/www/html/
+  wp core download --allow-root
+  mv ./wp-config.php /var/www/html/ #можно перенести в докерфайл
+  echo "Configuring Wordpress parameters"
+  wp core install --allow-root \
+      --url=localhost/wordpress/ \ #поменять ключи на $(something)
+      --title=MY_TITLE \
+      --admin_user=lmushroo \
+      --admin_password=lmushroo \
+      --admin_email=lmushroo@student.com
+#   wp user create --allow-root ${USER} ${EMAIL} --user_pass=${PASS}
+fi
+exec "$@"
